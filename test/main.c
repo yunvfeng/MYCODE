a13e6fa6736afbc3ad14971802616ff9b76508cd
yunfeng <swxsz813@icloud.com>
2025-11-14 14:25:07 +0800

    更新 main.c：添加了一条新的 printf 语句

--- test/main.c
+++ test/main.c
@@ -1,12 +1,17 @@
 // main.c
 #include <stdio.h>
-#include "math_utils.h" // 引用“承诺”
+#include "math_utils.h" 
 
 int main() {
     int x = 5;
     int y = 10;
-    int result = add(x, y); // 使用 "add" 函数
+    int result = add(x, y); 
 
     printf("The result is: %d\n", result);
+
+    // --- 添加这行新代码 ---
+    printf("Hello from GitHub! This is my first update.\n");
+    // ---------------------
+
     return 0;
 }
